b8a5cf387ace4e782c32b46f49f6ac2b57297a37
alonzofroman <alonzofroman@gmail.com>
2021-09-29 16:47:44 -0700

    Added timeblocks

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,14 @@
 $(document).ready(function () {
-
-    console.log(moment());
-
-
-    // Current Time
-    // Here we want to use setInterval to constantly update the time
-    let updateTime = function () {
-        let currentTime = moment().format('MMMM Do YYYY, h:mm:ss')
-        $("#currentDay").text(currentTime)
-    }
-   
-    // To initally set the times, we will call the functions
-    updateTime();
     
-    // To continuously call the functions, we will use setInterval
+    // Function to update to the current time when called
+    let updateTime = function () {
+        let currentTime = moment().format('MMMM Do YYYY, h:mm:ss') //Formatting the time
+        $("#currentDay").text(currentTime) //Displays the current time
+    }
+    updateTime();
+    // Update the time by calling the function every second
     setInterval(updateTime, 1000);
 
 })
+
+
